1bb550d113abf5cba673a8c4871b3282b678065c
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-02 16:41:28 +0200

    Initial Commit
    Unity files into git folder

---
+++ Frog Go Die/Assets/Scripts/Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	public Rigidbody2D rigBod;
+
+	public float minSpeed = 5;
+	public float maxSpeed = 10;
+
+	public float speed;
+
+	void Start(){
+		speed = Random.Range(minSpeed, maxSpeed);
+	}
+
+	void FixedUpdate(){
+		Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+
+		rigBod.MovePosition(rigBod.position + forward * Time.fixedDeltaTime * speed);
+	}
+}
---
+++ Frog Go Die/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public List<Sprite> cars = new List<Sprite>();
+
+	public GameObject car;
+
+	void Start(){
+		StartCoroutine("carSpawner");
+	}
+	
+	private IEnumerator carSpawner(){
+		Instantiate(car, transform.position, transform.rotation);
+		yield return new WaitForSeconds(2);
+	}
+}
