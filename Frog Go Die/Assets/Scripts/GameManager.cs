57023bc340034fecbf250e4d6937d5130ed7731f
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-10 12:58:29 +0200

    Added Second level
    Tiaem for snek

--- Frog Go Die/Assets/Scripts/GameManager.cs
+++ Frog Go Die/Assets/Scripts/GameManager.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2D(Collider2D other){
+		SceneManager.LoadScene(3);
 	}
 }
