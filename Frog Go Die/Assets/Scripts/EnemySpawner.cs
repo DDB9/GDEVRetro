4eda055bc7c1d6c083d63f4b26bc974ec402c623
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-11 17:42:29 +0200

    Updated EnemySpawner Script
    Added Spawner for third area.

--- Frog Go Die/Assets/Scripts/EnemySpawner.cs
+++ Frog Go Die/Assets/Scripts/EnemySpawner.cs
@@ -7,15 +7,15 @@ public class EnemySpawner : MonoBehaviour {
 
 	public static EnemySpawner instance = null;
 
-	public GameObject car;
-	public GameObject snake;
-	GameObject temp;
+	public GameObject car, snake, croc, log;
+
+	private GameObject temp;					// Temporary gameobject because Unity doesnt allow destruction of assets (and neither do I).
 
 	void Start(){
 		Scene currentScene = SceneManager.GetActiveScene();
 		int buildIndex = currentScene.buildIndex;
 
-		switch(buildIndex){
+		switch(buildIndex){								// This spawns enemies depending on the current scene's index.
 			case 1:
 				StartCoroutine(CarSpawner());
 				break;
@@ -43,6 +43,25 @@ public class EnemySpawner : MonoBehaviour {
 			temp = Instantiate(snake, transform.position, transform.rotation);
 			yield return new WaitForSeconds(1f);
 			Destroy(temp, 2);
+		}
+	}
+
+	private IEnumerator CrocLogSpawner(){											// Spawns Logs and Crocodiles at random.
+		int crocOrLog = Random.Range(1, 2);											// Wether a croc or a log is being spawned is chosen randomly.
+
+		if (crocOrLog == 1){														// Spawns a croc.
+			while (true){
+				temp = Instantiate(croc, transform.position, transform.rotation);
+				yield return new WaitForSeconds(1f);
+				Destroy(temp, 1f);
+			}
+		}
+		if (crocOrLog == 2){														// Spawns on log.
+			while (true){
+				temp = Instantiate(log, transform.position, transform.rotation);
+				yield return new WaitForSeconds(1f);
+				Destroy(temp, 1f);
+			}
 		}
 	}
 }
