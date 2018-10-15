2fa26144e1c4c94161296c89d5443e13ea4659ba
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-15 17:52:28 +0200

    Changes to 3rd scene
    - Added loads of water tiles for the player to drown in.
    - Moving logs remove the tiles so the player can move across the water

---
+++ Frog Go Die/Assets/Scripts/Lake Scripts/LogManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LogManager : MonoBehaviour {
+
+	IEnumerator OnTriggerStay2D(Collider2D other){
+		if (other.tag == "WaterTile"){
+			other.enabled = false;
+			Debug.Log("Destroyed");
+			yield return new WaitForSeconds(1f);
+			other.enabled = true;
+		}
+	}
+}
---
+++ Frog Go Die/Assets/Scripts/Player Scripts/PlayerDrowning.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerDrowning : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		SceneManager.LoadScene("GameOverLake");
+	}
+
+}
